201e42ffe87a7381d48d86edaa9b92d715357dbf
BmHaynes <39750888+BmHaynes@users.noreply.github.com>
2021-01-05 02:51:50 -0600

    Update algorithims.py

--- algorithims.py
+++ algorithims.py
@@ -1,7 +1,7 @@
 import datetime
-import BudgetingApp
-# tracking when to pay each bill
 import sqlite3 as sq
+
+# tracking when to pay each bill
 
 con = sq.connect("Budgetingapp.db")
 c = con.cursor()
@@ -13,72 +13,36 @@ def duedate():
     b = c.fetchall()
     for name, yn, date, amount in b:
         if yn == 'False':
-            print(name + " is not paid yet.")
+            #print(name + " is not paid yet.")
             currbill.append((name, date, amount))
 
-        if yn == 'True':
-            print(name + " has been paid this month.")
-    print(currbill)
+#        if yn == 'True':
+            #print(name + " has been paid this month.")
+    #print(currbill)
 
     pday = []
+    thisPD =[]
+    twoweeks = datetime.timedelta(days=14)
     c.execute("SELECT name, date, amount FROM paycheck")
     p = c.fetchall()
     for name, date, amount in p:
         pday.append((name, date, amount))
-        print(pday)
-        thisPD =[]
-        nextPD = []
-        tday = datetime.date.today()
-    for date in pday:
+    print(pday)
+    print(currbill)
 
-        if tday <= date:
-            thisPD.append((name, date, amount))
-    print(thisPD)
+
+
+
+    # for name, date, amount in currbill:
+    #     if currbill(1[1]) < pday(1[1]):
+    #         thisPD.append((name, date, amount))
+    # print(thisPD)
+
+
+
+
 
 duedate()
 
-# class Bill:
-#     def __init__(self, name, amount, expected_amount, date, completed):
-#         self.name = name
-#         self.amount = amount
-#         self. expected_amount = expected_amount
-#         self. date = date
-#         self.completed = completed
-#
-#
-# # taking the bills that have not been paid
-#
-#     def check_dates(self,name, date, amount, completed):
-#         first_list= []
-#         thisPD = []
-#         nextPD = []
-#         next_payday = "however you do biweekly stuff"
-#         for name,yn in BudgetingApp.paid():
-#             if yn == "False":
-#                 first_list.append[self.name,self.date]
-#             else:
-#                 pass
-#
-#             for name, notpaid in first_list:
-#         # if due date is before next payday put in this payday list
-#                 if notpaid => next_payday:
-#                     thisPD.append[name, self.amount]
-#                     return thisPD
-#         # else if due date is after next payday put in next payday list
-#                 elif notpaid =< next_payday:
-#                     nextPD.append[name, self.amount]
-#                     return nextPD
-#
-#     def what_to_pay(self,thisPD, nextPD):
-#         first= sum(thisPD[1])
-#         if first => budget:
-#             print(thisPD)
-#         # do the math to figure out which bills can wait to next payday
-#         else:
-#             pass
 
-
-# ordering by due date and amount
-
-
-# deciding which bill should be paid on which paycheck
+# I need to check to see if the date of
